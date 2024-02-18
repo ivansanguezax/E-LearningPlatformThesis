9464d7aadff3b1563d4edc3a8dce04c9ecea2942
ivansanguezax <ivansanguezax@gmail.com>
2024-02-18 13:24:39 -0400

    Crear pregunta - agregar respuesta y mandar por Email

--- server/routes/course.route.ts
+++ server/routes/course.route.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import { editCourse, getAllCourses, getCourseByUser, getSingleCourse, uploadCourse } from "../controllers/course.controller";
+import { addAnwser, addQuestion, editCourse, getAllCourses, getCourseByUser, getSingleCourse, uploadCourse } from "../controllers/course.controller";
 import { authorizeRoles, isAutheticated } from "../middleware/auth";
 const courseRouter = express.Router();
 
@@ -22,6 +22,10 @@ courseRouter.get("/get-course/:id", getSingleCourse);
 courseRouter.get("/get-courses", getAllCourses);
 
 courseRouter.get("/get-course-content/:id", isAutheticated, getCourseByUser);
+
+courseRouter.put("/add-question", isAutheticated, addQuestion);
+
+courseRouter.put("/add-answer", isAutheticated, addAnwser);
 
 
 export default courseRouter;
